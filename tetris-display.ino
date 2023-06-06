06649f2bcd3230707f60c5bcd6abd7e4151e4703
bvpav <bvpav@tutanota.com>
2023-06-06 17:36:38 +0300

    long time no refactor

--- tetris-display.ino
+++ tetris-display.ino
@@ -1,7 +1,8 @@
 #include <MD_MAX72xx.h>
 
-constexpr int DPY_WIDTH=8, DPY_HEIGHT=32;
-constexpr int DPY_CS_PIN=10; // Digital
+constexpr int GAME_DPY_WIDTH=8, GAME_DPY_HEIGHT=32;
+constexpr int PREVIEW_DPY_WIDTH=8, PREVIEW_DPY_HEIGHT=32;
+constexpr int GAME_DPY_CS_PIN=10, PREVIEW_DPY_PIN=8; // Digital
 constexpr int KEYPAD_OUT_PIN=0; // Analog
 constexpr int ENC_CLK_PIN=2, ENC_DT_PIN=3, ENC_SW_PIN=4; // Digital
 
@@ -49,7 +50,7 @@ struct Encoder
     pinMode(clk_pin, INPUT);
     pinMode(dt_pin, INPUT);
     pinMode(sw_pin, INPUT_PULLUP);
-    last_clk = digitalRead(clk_pin); 
+    last_clk = digitalRead(clk_pin);
   }
 
   void read()
@@ -66,7 +67,7 @@ struct Encoder
     else
       has_rotated = false;
     last_clk = current_clk;
-  
+
     bool new_button = !digitalRead(sw_pin);
     was_button_pressed = !button && new_button;
     button = new_button;
@@ -79,7 +80,8 @@ struct Encoder
   }
 };
 
-MD_MAX72XX dpy(MD_MAX72XX::FC16_HW, DPY_CS_PIN, DPY_HEIGHT/8);
+MD_MAX72XX game_dpy(MD_MAX72XX::FC16_HW, GAME_DPY_CS_PIN, GAME_DPY_HEIGHT/8);
+MD_MAX72XX preview_dpy(MD_MAX72XX::FC16_HW, GAME_DPY_CS_PIN, GAME_DPY_HEIGHT/8);
 Encoder enc(ENC_CLK_PIN, ENC_DT_PIN, ENC_SW_PIN);
 
 constexpr char *tetrominoes[] =
@@ -121,7 +123,8 @@ constexpr char *tetrominoes[] =
 };
 constexpr size_t num_tetrominoes = sizeof tetrominoes / sizeof *tetrominoes;
 
-const char (*rotations[])(int dx, int dy, int tetromino_idx) = {
+const char (*rotations[])(int dx, int dy, int tetromino_idx) =
+{
   [tetrominoes](int dx, int dy, int tetromino_idx) { return tetrominoes[tetromino_idx][dy * 4 + dx]; },
   [tetrominoes](int dx, int dy, int tetromino_idx) { return tetrominoes[tetromino_idx][12 + dy - (dx * 4)]; },
   [tetrominoes](int dx, int dy, int tetromino_idx) { return tetrominoes[tetromino_idx][15 - (dy * 4) - dx]; },
@@ -129,6 +132,7 @@ const char (*rotations[])(int dx, int dy, int tetromino_idx) = {
 };
 constexpr size_t num_rotations = sizeof rotations / sizeof *rotations;
 
+bool stack[GAME_DPY_WIDTH][GAME_DPY_HEIGHT]{false};
 
 struct Tetromino
 {
@@ -145,91 +149,131 @@ struct Tetromino
   {
     return rotations[rotation](dx, dy, type) == 'X';
   }
+
+  bool can_exist()
+  {
+    for (int dx=0; dx<4; ++dx)
+      for (int dy=0; dy<4; ++dy)
+        if (get_pos(dx, dy) && !is_pos_free(x+dx, y+dy))
+          return false;
+    return true;
+  }
+
+  void rotate()
+  {
+    int new_piece_rot = (rotation + 1) % num_rotations;
+    if (Tetromino(x, y, type, new_piece_rot).can_exist())
+    {
+      rotation = new_piece_rot;
+      // XXX: maybe don't redraw here, but outside??
+      redraw();
+    }
+  }
+
+  void apply_gravity()
+  {
+    if (Tetromino(x, y+1, type, rotation).can_exist())
+    {
+      ++y;
+      redraw();
+    }
+    else
+      place();
+  }
+
+  void apply_gravity_timed()
+  {
+    static unsigned long last_update = millis();
+    unsigned long now = millis();
+    if (now - last_update < 1000)
+      return;
+    last_update = now;
+    apply_gravity();
+  }
+
+  void move_left()
+  {
+    if (Tetromino(x-1, y, type, rotation).can_exist())
+    {
+      --x;
+      redraw();
+    }
+  }
+
+  void move_right()
+  {
+    if (Tetromino(x+1, y, type, rotation).can_exist())
+    {
+      ++x;
+      redraw();
+    }
+  }
+
+  void drop()
+  {
+    while (Tetromino(x, y+1, type, rotation).can_exist())
+      ++y;
+  }
+
+  void place()
+  {
+    for (int dx=0; dx<4; ++dx)
+        for (int dy=0; dy<4; ++dy)
+          if (get_pos(dx, dy))
+            stack[x+dx][y+dy] = true;
+    for (int y=GAME_DPY_HEIGHT-1, offset=0; 0 <= y; --y)
+    {
+      if (stack[0][y] && stack[0][y] == stack[1][y] && stack[1][y] == stack[2][y] && stack[2][y] == stack[3][y] &&
+            stack[3][y] == stack[4][y] && stack[4][y] == stack[5][y] && stack[5][y] == stack[6][y] && stack[6][y] == stack[7][y])
+        ++offset;
+      else
+        for (int x=0; x<GAME_DPY_WIDTH; ++x)
+          stack[x][y+offset] = stack[x][y];
+    }
+    x = y = rotation = 0;
+    type = (type + 1) % num_tetrominoes;
+  }
 } tetromino;
 
 bool is_gravity_enabled=false;
 
-bool stack[DPY_WIDTH][DPY_HEIGHT]{false};
-
-void draw_tetromino(const struct Tetromino &t)
+void draw_tetromino(MD_MAX72XX &dpy, const struct Tetromino &t)
 {
   for (int dx=0; dx<4; ++dx)
     for (int dy=0; dy<4; ++dy)
-      dpy.setPoint(DPY_WIDTH-t.x-1-dx, DPY_HEIGHT-t.y-1-dy, t.get_pos(dx, dy));
+      dpy.setPoint(GAME_DPY_WIDTH-t.x-1-dx, GAME_DPY_HEIGHT-t.y-1-dy, t.get_pos(dx, dy));
 }
 
 void draw_stack()
 {
-  for (int x=0; x<DPY_WIDTH; ++x)
-    for (int y=0; y<DPY_HEIGHT; ++y)
+  for (int x=0; x<GAME_DPY_WIDTH; ++x)
+    for (int y=0; y<GAME_DPY_HEIGHT; ++y)
       if (stack[x][y])
-        dpy.setPoint(DPY_WIDTH-x-1, DPY_HEIGHT-y-1, true);
+        game_dpy.setPoint(GAME_DPY_WIDTH-x-1, GAME_DPY_HEIGHT-y-1, true);
 }
 
 bool is_pos_free(int x, int y)
 {
-  return 0 <= x && x < DPY_WIDTH &&
-    0 <= y && y < DPY_HEIGHT &&
+  return 0 <= x && x < GAME_DPY_WIDTH &&
+    0 <= y && y < GAME_DPY_HEIGHT &&
     !stack[x][y];
 }
 
 // TODO: make method of Tetromino
-bool can_have_tetromino(const struct Tetromino &t)
-{
-  for (int dx=0; dx<4; ++dx)
-    for (int dy=0; dy<4; ++dy)
-      if (t.get_pos(dx, dy) && !is_pos_free(t.x+dx, t.y+dy))
-        return false;
-  return true;
-}
 
-// TODO: make method of Tetromino
-void place_piece()
-{
-  for (int dx=0; dx<4; ++dx)
-      for (int dy=0; dy<4; ++dy)
-        if (tetromino.get_pos(dx, dy))
-          stack[tetromino.x+dx][tetromino.y+dy] = true;
-  for (int y=DPY_HEIGHT-1, offset=0; 0 <= y; --y)
-  {
-    if (stack[0][y] && stack[0][y] == stack[1][y] && stack[1][y] == stack[2][y] && stack[2][y] == stack[3][y] &&
-          stack[3][y] == stack[4][y] && stack[4][y] == stack[5][y] && stack[5][y] == stack[6][y] && stack[6][y] == stack[7][y])
-      ++offset;
-    else
-      for (int x=0; x<DPY_WIDTH; ++x)
-        stack[x][y+offset] = stack[x][y];
-  }
-  tetromino.x = tetromino.y = tetromino.rotation = 0;
-  tetromino.type = (tetromino.type + 1) % num_tetrominoes;
-}
-
-// TODO: make method of Tetromino
-void apply_gravity()
-{
-  static unsigned long last_update = millis();
-  unsigned long now = millis();
-  if (now - last_update < 1000)
-    return;
-  last_update = now;
-  
-  if (can_have_tetromino(tetromino))
-    ++tetromino.y;
-  else
-    place_piece();
-  redraw();
-}
 
 void redraw()
 {
-  dpy.clear();
-  draw_tetromino(tetromino);
+  game_dpy.clear();
+  draw_tetromino(game_dpy, tetromino);
   draw_stack();
 }
 
 void setup()
 {
   Serial.begin(9600);
-  dpy.begin();
+  game_dpy.begin();
+  preview_dpy.begin();
   enc.begin();
   redraw();
 }
@@ -242,39 +286,20 @@ void loop()
     switch (get_pressed_key(KEYPAD_OUT_PIN))
     {
       case Key::Up:
-      {
-        int new_piece_rot = (tetromino.rotation + 1) % num_rotations;
-        if (can_have_tetromino(Tetromino(tetromino.x, tetromino.y, tetromino.type, new_piece_rot)))
-        {
-          tetromino.rotation = new_piece_rot;
-          redraw();
-        }     
-      } break;
+        tetromino.rotate();
+        break;
       case Key::Down:
-        if (can_have_tetromino(Tetromino(tetromino.x, tetromino.y+1, tetromino.type, tetromino.rotation)))
-        {
-          ++tetromino.y;
-          redraw();
-        }
+        tetromino.apply_gravity();
         break;
       case Key::Left:
-        if (can_have_tetromino(Tetromino(tetromino.x-1, tetromino.y, tetromino.type, tetromino.rotation)))
-        {
-          --tetromino.x;
-          redraw();
-        }
+        tetromino.move_left();
         break;
       case Key::Right:
-        if (can_have_tetromino(Tetromino(tetromino.x+1, tetromino.y, tetromino.type, tetromino.rotation)))
-        {
-          ++tetromino.x;
-          redraw();
-        }
+        tetromino.move_right();
         break;
       case Key::Drop:
-        while (can_have_tetromino(Tetromino(tetromino.x, tetromino.y+1, tetromino.type, tetromino.rotation)))
-            ++tetromino.y;
-        place_piece();
+        tetromino.drop();
+        tetromino.place();
         break;
     }
   enc.read();
@@ -283,6 +308,6 @@ void loop()
   if (enc.was_button_pressed)
     ((void (*)())0)(); // reset arduino
   if (is_gravity_enabled)
-    apply_gravity();
+    tetromino.apply_gravity_timed();
   delay(1);
 }
